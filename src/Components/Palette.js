0301313f63e53aaf01bfc96943a8d16f63415e20
liamH47 <liamhanafee@gmail.com>
2021-01-19 10:05:07 -0500

    colors rendering

--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import ColorTile from './ColorTile'
+import '../StyleSheets/Palette.css'
 
 class Palette extends Component {
+
+    // componentDidMount() {
+    //     console.log(this.props.palette);
+    //     debugger
+    // }
+    
+
     render() {
+        const colorTiles = this.props.palette.colors.map(color => (<ColorTile background={color.color} name={color.name} />))
         return (
             <div className='Palette' >
-                <div className='Palette-colors'></div>
+                <div className='Palette-colors'>
+                    {colorTiles}
+                </div>
             </div>
         );
     }
